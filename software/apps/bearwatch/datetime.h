6ac59c346d1b3b1b403707f7afe5469557492f8f
arvinds <arvind0sundararajan@gmail.com>
2018-12-09 18:07:11 -0800

    final version of datetime

--- software/apps/bearwatch/datetime.h
+++ software/apps/bearwatch/datetime.h
@@ -1,8 +1,8 @@
 #ifndef DATETIME__
 #define DATETIME__
 
-extern time_t *current_time; //time_t object of current time (seconds past epoch)
-extern struct tm *current_time_info; //datetime information struct
+time_t *initial_time; //time_t object of current time (seconds past epoch)
+struct tm *current_time_info; //datetime information struct
 
 /* increments the time by 1 s.*/
 void update_time(void);
