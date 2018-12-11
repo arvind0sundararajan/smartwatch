da366209cdb29bb4574345e520b46ab25d0dabee
Hyunkyu Lee <hyunkyu@berkeley.edu>
2018-12-10 17:40:21 -0800

    adds datetime service to the manager file

--- software/apps/bearwatch/smartwatch_ble_service_manager.h
+++ software/apps/bearwatch/smartwatch_ble_service_manager.h
@@ -22,8 +22,11 @@
 #include "smartwatch_pressure_service.h"
 #include "smartwatch_temp_service.h"
 #include "smartwatch_humidity_service.h"
+#include "smartwatch_datetime_service.h"
 
-smartwatch_ble_service* custom_services[5];
+#define NUM_SERVICES NRF_SDH_BLE_VS_UUID_COUNT
+
+smartwatch_ble_service* custom_services[NUM_SERVICES];
 
 void services_init(void);
 void manager_evt_handler(ble_evt_t const * p_ble_evt, void * p_context);
